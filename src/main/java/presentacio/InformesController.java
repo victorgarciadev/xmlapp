3eca165d25e5c40d27e9a6d8d818d51ae7682d63
Izan Jimenez <soyizanjimenez@gmail.com>
2023-01-21 13:47:32 +0100

    21/01/2023 -> informes.fxml + InformesController.java

---
+++ src/main/java/presentacio/InformesController.java
@@ -0,0 +1,156 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package presentacio;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Izan
+ */
+public class InformesController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private VBox container;
+    @FXML
+    private MenuBar menu;
+    @FXML
+    private MenuItem menuitem_importar, menuitem_sortir, menuitem_llistar, menuitem_informes;
+    @FXML
+    private Menu menu_registres, menu_informes, menu_arxiu;
+    @FXML
+    private ComboBox<?> selector_ordenar;
+    @FXML
+    private Button btnExportarInforme;
+    @FXML
+    private BarChart<?, ?> barChartEvolució;
+    @FXML
+    private PieChart cake;
+    @FXML
+    private Label maxPoblacio, minPoblació;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        //importar dades a la cacke
+        ObservableList<PieChart.Data> cackeData
+                = FXCollections.observableArrayList(new PieChart.Data("HOMES", 40), new PieChart.Data("DONES", 60));
+        cake.setData(cackeData);
+
+        //importar dades al barChart
+        XYChart.Series dada1 = new XYChart.Series();
+        dada1.setName("Primera dada");
+        dada1.getData().add(new XYChart.Data("2009", 10000));
+        dada1.getData().add(new XYChart.Data("2010", 9000));
+        dada1.getData().add(new XYChart.Data("2011", 14400));
+        dada1.getData().add(new XYChart.Data("2012", 3500));
+        
+        barChartEvolució.getData().addAll(dada1);
+
+    }
+
+    /**
+     * Porta a l'apartat 'Importar XML', que permet a l'usuari obrir un nou
+     * arxiu XML amb el que treballar.
+     *
+     * @param event Acció que afecti al 'MenuItem' (ex: clicar)
+     * @throws IOException Excepció a mostrar en cas que no es trobi el Layout
+     * @author Izan Jimenez - Implementació
+     */
+    @FXML
+    private void pantalla_importar(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/presentacio/importar.fxml"));
+        stage = (Stage) (container.getScene().getWindow());
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Dades demogràfiques de catalans a l'estranger > Importar XML");
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    /**
+     * Tanca l'aplicació.
+     *
+     * @param event Acció que afecti al 'MenuItem' (ex: clicar)
+     *
+     * @author Izan Jimenez - Implementació
+     */
+    @FXML
+    private void tancar_aplicacio(ActionEvent event) {
+        ((Stage) container.getScene().getWindow()).close();
+    }
+
+    /**
+     * Porta a l'apartat 'Llistat de registres', que permet a l'usuari
+     * visualitzar, cercar, ordenar i exportar els registres de l'XML carrgeat.
+     *
+     * @param event Acció que afecti al 'MenuItem' (ex: clicar)
+     * @throws IOException Excepció a mostrar en cas que no es trobi el Layout
+     * @author Izan Jimenez - Implementació
+     */
+    @FXML
+    private void pantalla_registres(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/presentacio/registres.fxml"));
+        stage = (Stage) (container.getScene().getWindow());
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Dades demogràfiques de catalans a l'estranger > Llistat de registres");
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    /**
+     * Porta a l'apartat 'Informes', que permet a l'usuari visualitzar un
+     * conjunt de gràfiques i estadístiques de l'XML carrgeat.
+     *
+     * @param event Acció que afecti al 'MenuItem' (ex: clicar)
+     * @throws IOException Excepció a mostrar en cas que no es trobi el Layout
+     * @author Izan Jimenez - Implementació
+     */
+    @FXML
+    private void pantalla_informes(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/presentacio/informes.fxml"));
+        stage = (Stage) (container.getScene().getWindow());
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("DEMOGRAFIA A L'EXTRANGER. Informes");
+        stage.sizeToScene();
+        stage.show();
+    }
+
+    @FXML
+    private void btnExportarInformePressed(ActionEvent event) {
+    }
+
+}
